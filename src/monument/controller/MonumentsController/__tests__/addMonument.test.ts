a8fca8b02de0cb018c997189172915a2aecc693d
Clara <clarafdezfraga@gmail.com>
2024-05-11 19:08:06 +0200

    Test same name existing monument when adding  monument in the controller

--- src/monument/controller/MonumentsController/__tests__/addMonument.test.ts
+++ src/monument/controller/MonumentsController/__tests__/addMonument.test.ts
@@ -1,12 +1,14 @@
 import { type NextFunction, type Response } from "express";
 import Monument from "../../../Monument/Monument";
 import MonumentsController from "../MonumentsController";
-import type InMemoryMonumentsRepository from "../../../repository/InMemoryMonumentRepository";
+import InMemoryMonumentsRepository from "../../../repository/InMemoryMonumentRepository";
 import {
   type PartialRequestWithMonumentWithoutId,
   type RequestWithMonumentBodyWithoutId,
 } from "../types";
 import { monumentsController } from "../..";
+import ServerError from "../../../../server/middlewares/errors/ServerError/ServerError";
+import monuments from "../../../data";
 
 describe("Given the  monumentsConroller addMonument method", () => {
   const res: Pick<Response, "status" | "json"> = {
@@ -22,29 +24,29 @@ describe("Given the  monumentsConroller addMonument method", () => {
       imageUrl: "url",
     },
   };
-  const next: NextFunction = jest.fn().mockReturnThis();
+  const next = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   const monumentData: Omit<Monument, "id"> = {
-    name: "Sagrada familia",
+    name: "Sagrada Familia",
     description: "templo sin acabar",
     imageUrl: "url",
     city: "BCN",
     country: "España",
   };
 
+  const sagradaFamilia = new Monument(
+    monumentData.name,
+    monumentData.description,
+    monumentData.imageUrl,
+    { city: monumentData.city, country: monumentData.country },
+  );
+
   describe("When it receives a request with 'Sagrada Familia','templo sin acabar', 'url' 'BCN' y 'España'", () => {
     test("Then it should call the response method with  the 'Sagrada familia' monument", async () => {
-      const sagradaFamilia = new Monument(
-        monumentData.name,
-        monumentData.description,
-        monumentData.imageUrl,
-        { city: monumentData.city, country: monumentData.country },
-      );
-
       const monumentsRepository: InMemoryMonumentsRepository = {
         monuments: [],
         getAll: jest.fn(),
@@ -56,7 +58,7 @@ describe("Given the  monumentsConroller addMonument method", () => {
       await monumentsController.addMonument(
         req as RequestWithMonumentBodyWithoutId,
         res as Response,
-        next,
+        next as NextFunction,
       );
 
       expect(res.json).toHaveBeenCalledWith({
@@ -74,6 +76,42 @@ describe("Given the  monumentsConroller addMonument method", () => {
       );
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+  });
+
+  describe("When it receives a request with 'Templo Miau','templo de gattos', 'url' 'Aviles' y 'España' and there's a Templo miau in the list", () => {
+    test("Then it should call the next function with the error'Monument named 'Templo Miau' already exists' with the statusCode 409 ", async () => {
+      const req: PartialRequestWithMonumentWithoutId = {
+        body: {
+          name: "Templo Miau",
+          description: "templo de gatos",
+          city: "BCN",
+          country: "España",
+          imageUrl: "url",
+        },
+      };
+      const monuments = [
+        new Monument("Templo miau", "templo de gatos", "url", {
+          city: "aviles",
+          country: "españa",
+        }),
+      ];
+
+      const monumentsRepository = new InMemoryMonumentsRepository(monuments);
+
+      const monumentsController = new MonumentsController(monumentsRepository);
+
+      const errorMessage = "Monument named 'Templo Miau' already exists";
+      const statusCode = 409;
+      const expectedError = new ServerError(errorMessage, statusCode);
+
+      await monumentsController.addMonument(
+        req as RequestWithMonumentBodyWithoutId,
+        res as Response,
+        next,
+      );
+
+      expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
 });
